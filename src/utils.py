c42bba1fb7ceaee461ade0c7d1ba0d83c51a3ba4
Colin S <colinsf@me.com>
2014-08-27 21:49:18 -0700

    add utils module and tests. stub first challenge method.

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+""" This utilities file holds important (and hopefully reusable) functions for 
+    the Matasano Cryptopals challenges.
+"""
+
+##** Constants
+
+# This table contains a list of the base64 encoding character values.
+BASE_64_TABLE = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
+    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", 
+    "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", 
+    "4", "5", "6", "7", "8", "9", "+", "/"
+]
+
+##** Functions
+
+def hex_to_base64(hex_string):
+    """ This function takes a hexadecimal string for input and converts it to 
+        base64.
+
+        http://en.wikipedia.org/wiki/Base64
+    """
+    
+    return "plugh"
