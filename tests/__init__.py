8d511a5e279cf3e9177b3b9a44af5cc998d2f710
Colin S <colinsf@me.com>
2014-08-28 19:00:20 -0700

    cleanup tests init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,13 @@
-import unittest
+# import unittest
 
-from tests.utils_tests import *
+# from tests.utils_tests import *
 
-utils_suite = utils_tests.suite()
+# # Add new test_module.suite() calls here.
+# utils_suite = utils_tests.suite()
+
+# # Add all new test suites to this object.
+# suite = unittest.TestSuite(
+#     [utils_suite]
+# )
+
+# unittest.TextTestRunner(verbosity=2).run(suite)
